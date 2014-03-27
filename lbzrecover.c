27cc4ae2ebf2ea90b8a215c14e792ebef2537dea
Mikolaj Izdebski <zurgunt@gmail.com>
2014-03-27 01:38:13 +0100

    Include config.h in lbrecover

--- lbzrecover.c
+++ lbzrecover.c
@@ -1,5 +1,5 @@
 /*-
-  Copyright (C) 2013 Mikolaj Izdebski
+  Copyright (C) 2013, 2014 Mikolaj Izdebski
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -14,6 +14,10 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
 
 #include <errno.h>
 #include <inttypes.h>
